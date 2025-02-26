42c19b73da69b798388417de119275351f415209
ReinoutWW <71006243+ReinoutWW@users.noreply.github.com>
2025-02-26 23:17:55 +0100

    Create IUsageQueryService.cs

---
+++ MethodQueryUsageCodeLensProvider/IUsageQueryService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodQueryUsageCodeLensProvider
+{
+    public interface IUsageQueryService
+    {
+        /// <summary>
+        /// Fetches usage/performance data for a given method signature.
+        /// e.g. "MyNamespace.MyClass.MyMethod"
+        /// </summary>
+        Task<QueryPerformanceDetails> GetMethodPerformanceDetailsAsync(string methodSignature);
+    }
+}
